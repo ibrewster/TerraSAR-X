268262853831dd34aab570a350aae4b2ee0ed450
Israel Brewster <ijbrewster@alaska.edu>
2024-04-30 10:04:17 -0800

    Add hash bang line to fetch script

--- fetchImages.sh
+++ fetchImages.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 /apps/TerraSAR-X/tsx-env/bin/python /apps/TerraSAR-X/SAR.py
 rsync -r /geodesy/data/TerraSAR-X/archive/zip/ root@akutan.avo.alaska.edu:/ftp_home/TerraSAR-X/
 rm -r /geodesy/data/TerraSAR-X/archive/zip/*
