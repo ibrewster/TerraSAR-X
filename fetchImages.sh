c26f0a6f5c040048fa43d47569e919a68edd77f9
Israel Brewster <ijbrewster@alaska.edu>
2024-05-07 18:40:40 +0000

    rsync zip files into sub-directory

--- fetchImages.sh
+++ fetchImages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 /apps/TerraSAR-X/tsx-env/bin/python /apps/TerraSAR-X/SAR.py
-rsync -r /geodesy/data/TerraSAR-X/archive/zip/ root@akutan.avo.alaska.edu:/ftp_home/TerraSAR-X/
+rsync -r /geodesy/data/TerraSAR-X/archive/zip root@akutan.avo.alaska.edu:/ftp_home/TerraSAR-X/
 rsync -r /geodesy/data/TerraSAR-X/archive/cropped root@akutan.avo.alaska.edu:/ftp_home/TerraSAR-X/
 rm -r /geodesy/data/TerraSAR-X/archive/zip/*
